d8f8129df13e20670ec49c66d5f81b039632892c
Thomas Campbell <thomasjaycampbell@gmail.com>
2018-04-24 15:04:11 -0400

    Initial

---
+++ Desktop/script.js
@@ -0,0 +1,20 @@
+
+    $( "#clickme1" ).click(function() {
+        $( "#sample1" ).toggle( function() {
+        });
+    });
+
+    $( "#clickme2" ).click(function() {
+        $( "#sample2" ).toggle( function() {
+        });
+    });
+
+        $( "#clickme3" ).click(function() {
+        $( "#sample3" ).toggle( function() {
+        });
+    });
+
+    $(function () {
+        $("#footerAtBottomId").load("footer.html");
+    });
+
